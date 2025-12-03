f6f6f3bfd4ab8a3ccba6b0cf5454187d4cf3e55d
phuongtuan1803 <phuongtuan1803@gmail.com>
2025-12-04 01:06:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@ python.exe -m pip install --upgrade pip
 pip install vastai
 
 vastai search offers 'gpu_name=RTX_4090 reliability>0.98 num_gpus=1 inet_down>400 disk_space>60' --order 'dph_total'
+
+vastai create instance 1234567 \
+    --image pytorch/pytorch:2.3.0-cuda12.1-cudnn8-devel \
+    --disk 60 \
+    --env "-e HF_TOKEN=hf_xxxxxx" \
+    --onstart-cmd "curl -sL https://gist.githubusercontent.com/LINK_TO_YOUR_RAW_SCRIPT.sh | bash" \
+    --ssh --direct
